0beb121f1fe30b9d24c80441cc6793332e62cdc1
camicerquetti <camilacerquetti18@gmail.com>
2024-08-31 21:29:39 -0300

    sin archivos estaticos

--- server.js
+++ server.js
@@ -19,25 +19,6 @@ app.use(express.json()); // Para manejar solicitudes JSON
 // Usa las rutas del usuario antes de servir archivos estáticos
 app.use('/api', userRoutes); // '/api' es el prefijo para tus rutas
 
-// Define el directorio de archivos estáticos
-const staticPath = path.join(__dirname, '../front-end/build');
-
-// Verifica la ruta de los archivos estáticos
-console.log('Serving static files from:', staticPath);
-
-// Sirve archivos estáticos desde la carpeta build del frontend
-app.use(express.static(staticPath));
-
-// Ruta para manejar el acceso a la SPA (Single Page Application)
-app.get('*', (req, res) => {
-  const filePath = path.join(staticPath, 'index.html');
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error('Error sending file:', err);
-      res.status(err.status || 500).send('Something went wrong!');
-    }
-  });
-});
 
 // Manejo de errores
 app.use((err, req, res, next) => {
