0aec8d76b8f64bc29f6b9926f7f7f34423f67207
camicerquetti <camilacerquetti18@gmail.com>
2024-08-31 21:25:31 -0300

    nuevo

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const db = require('./config/db'); 
 const userRoutes = require('./routes/userRoutes');
+const app = express();
+const port = process.env.PORT || 3001; // Usa el puerto del entorno o un puerto por defecto
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 3001;
-
-// Configura CORS para permitir solicitudes desde tu frontend
+// Configura CORS
 app.use(cors({
-  origin: 'https://frontcriptomate-1.onrender.com', // La URL donde está alojado tu frontend
+  origin: 'https://frontcriptomate-1.onrender.com', // Permite solicitudes desde este origen
 }));
 
 // Configura el middleware para manejar JSON
-app.use(express.json());
+app.use(express.json()); // Para manejar solicitudes JSON
 
-// Usa las rutas del usuario
-app.use('/api', userRoutes);
+// Usa las rutas del usuario antes de servir archivos estáticos
+app.use('/api', userRoutes); // '/api' es el prefijo para tus rutas
+
+// Define el directorio de archivos estáticos
+const staticPath = path.join(__dirname, '../front-end/build');
+
+// Verifica la ruta de los archivos estáticos
+console.log('Serving static files from:', staticPath);
+
+// Sirve archivos estáticos desde la carpeta build del frontend
+app.use(express.static(staticPath));
+
+// Ruta para manejar el acceso a la SPA (Single Page Application)
+app.get('*', (req, res) => {
+  const filePath = path.join(staticPath, 'index.html');
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      res.status(err.status || 500).send('Something went wrong!');
+    }
+  });
+});
 
 // Manejo de errores
 app.use((err, req, res, next) => {
