7146690a01a0fa4b33a883d84584d916c4d0d64d
camicerquetti <camilacerquetti18@gmail.com>
2024-09-04 23:25:11 -0300

    conexion exitosa

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+require('dotenv').config();
 
-// Configura la conexión a la base de datos usando variables de entorno
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,  
+  port: parseInt(process.env.DB_PORT, 10) || 3306, // Usa 3306 por defecto si no está especificado
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
@@ -12,5 +11,15 @@ const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// Test de conexión
+db.getConnection()
+  .then(connection => {
+    console.log('Conexión a la base de datos exitosa');
+    connection.release(); // Libera la conexión
+  })
+  .catch(err => {
+    console.error('Error de conexión a la base de datos:', err.message);
+  });
 
 module.exports = db;
